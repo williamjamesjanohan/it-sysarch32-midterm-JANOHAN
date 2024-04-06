831781c8e84ad1c8cde496885ecc007077dc8bd9
William <williamjamesjanohan1997@gmail.com>
2024-04-06 14:38:37 +0800

    Products CRUD

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,6 +4,18 @@ const mongoose = require('mongoose')
 const Product = require('../models/product')
 
 router.get(`/`, (req, res, next)=>{
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs)
+        res.status(200).json(docs)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    })
     res.status(200).json({
         message:"handling GET request to /products"
     })
@@ -34,17 +46,33 @@ router.get(`/:productId`, (req, res, next)=>{
     const id = req.params.productId
     Product.findById(id).exec()
     .then(doc => {
-        console.log(doc)
+        console.log("From database", doc)
         res.status(200).json(doc)
     })
     .catch(err =>{
         console.log(err)
-        res.status(500).json({error:err})
+        res.status(500).json({message : "No valid entry found",error:err})
     })
     
 })
 
 router.patch(`/:productId`, (req, res, next)=>{
+    const id = req.params.productId
+    const updateOps = {}
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value
+    }
+    Product.update({_id:id}, { $set: updateOps})
+        .exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err =>{
+            res.status(500).json({
+                error:err
+            })
+        })
     res.status(200).json({
         message:"Update product",
     })
@@ -56,7 +84,7 @@ router.delete(`/:productId`, (req, res, next)=>{
     Product.remove({_id:id})
     .exec()
     .then(result =>{
-        res.status(200).json()
+        res.status(200).json(result)
     })
     .catch(err=>{
         res.status(500).json({
